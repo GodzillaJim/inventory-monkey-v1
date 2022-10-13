ce09d106ef65f1a2acf58ff032a89a98f362c569
GodzillaJim <47969657+GodzillaJim@users.noreply.github.com>
2022-10-13 19:06:05 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from "react";
+import "./styles.scss";
+import "semantic-ui-css/semantic.min.css";
+
+const App: FunctionComponent = () => {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  );
+};
+
+export default App;
